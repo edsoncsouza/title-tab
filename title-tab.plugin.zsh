b4b2cf2cf1c237032541ac0e52b9fc5ae0400481
edsoncsouza <edson.souza@chaordicsystems.com>
2018-01-29 10:12:01 -0200

    Add ITERM_TITLE

--- title-tab.plugin.zsh
+++ title-tab.plugin.zsh
@@ -1,9 +1,21 @@
 function git_branch {
   BRANCH_REFS=$(git symbolic-ref HEAD 2>/dev/null) || return
   GIT_BRANCH="${BRANCH_REFS#refs/heads/}"
-  [ -n "$GIT_BRANCH" ] && echo "($GIT_BRANCH)"
+  [ -n "$GIT_BRANCH" ] && echo "$GIT_BRANCH"
+}
+
+function git_root {
+  GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null) || return
+  GIT_ROOT=${GIT_ROOT//*\//}
+  echo "$GIT_ROOT"
 }
 
 function precmd {
-  echo -ne "\e]1;${PWD//*\//} $(git_branch)\a"
+  if [ ! -z ${ITERM_TITLE+x} ]; then
+    TITLE=${ITERM_TITLE}
+  else
+    TITLE="$(git_root)"
+    [ "$TITLE" = "" ] && TITLE=${PWD//*\//}
+  fi
+  echo -ne "\e]1;$TITLE\a"
 }
